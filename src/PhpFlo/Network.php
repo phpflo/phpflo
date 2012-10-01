67aad22d04595045a9f86ffa9128364a98b3bc00
Marijn Huizendveld <marijn.huizendveld@gmail.com>
2012-10-01 09:33:23 +0200

    Report uptime with a timestamp resolution that includes miliseconds.

--- src/PhpFlo/Network.php
+++ src/PhpFlo/Network.php
@@ -11,7 +11,7 @@ class Network
     public function __construct(Graph $graph)
     {
         $this->graph = $graph;
-        $this->startupDate = new \DateTime();
+        $this->startupDate = $this->createDateTimeWithMilliseconds();
 
         $this->graph->on('addNode', array($this, 'addNode'));
         $this->graph->on('removeNode', array($this, 'removeNode'));
@@ -21,7 +21,7 @@ class Network
 
     public function uptime()
     {
-        return $this->startupDate->diff(new \DateTime());
+        return $this->startupDate->diff($this->createDateTimeWithMilliseconds());
     }
 
     public function addNode(array $node)
@@ -180,5 +180,10 @@ class Network
     {
         $graph = Graph::loadFile($file);
         return Network::create($graph);
+    }
+
+    private function createDateTimeWithMilliseconds()
+    {
+        return DateTime::createFromFormat('U.u', sprintf('%.6f', microtime(true)));
     }
 }
