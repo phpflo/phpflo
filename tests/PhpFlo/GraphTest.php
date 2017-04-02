c015d953b017cb137f4a1056d2cf67555a9da63f
Marc Aschmann <marc@aschmann.org>
2017-04-02 20:46:36 +0000

    improve graph tests

--- tests/PhpFlo/GraphTest.php
+++ tests/PhpFlo/GraphTest.php
@@ -14,6 +14,6 @@ class GraphTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(4, count($graph->nodes));
 
         $this->assertEquals(null, $graph->getNode('non_existing_node'));
-        
+
     }
 }
